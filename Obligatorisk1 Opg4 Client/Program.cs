2a7f47a61b9513ce94a631921666970597f31e40
Jonas Grønquist <jog002@edu.zealand.dk>
2024-09-30 10:11:04 +0200

    Add project files.

---
+++ Obligatorisk1 Opg4 Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+
+Console.WriteLine("Client");
+
+TcpClient socket = new TcpClient("127.0.0.1", 7);
+NetworkStream ns = socket.GetStream();
+
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+
+bool keepSending = true;
+
+while (keepSending)
+{
+    Console.Write("Enter message ('stop' to exit): ");
+    string message = Console.ReadLine();
+
+    if (message.ToLower() == "stop")
+    {
+        keepSending = false;
+        continue;
+    }
+
+    writer.WriteLine(message);
+    writer.Flush();
+
+    string response = reader.ReadLine();
+    if (response != null)
+    {
+        Console.WriteLine($"Server: {response}");
+    }
+    else
+    {
+        Console.WriteLine("No response received.");
+    }
+
+    if (!socket.Connected)
+    {
+        Console.WriteLine("Connection closed");
+        break;
+    }
+}
+
+socket.Close();
+
